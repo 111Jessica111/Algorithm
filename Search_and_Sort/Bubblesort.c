4e1d89ce45275a59b7ffeea3ef8b5c96d107024e
111Jessica111 <165466969+111Jessica111@users.noreply.github.com>
2024-09-07 17:24:53 +0800

    1

---
+++ Search_and_Sort/Bubblesort.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void swap(int* a, int* b) {
+	int temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+void Bubblesort(int A[], int length) {
+	int i, j;
+	for (i = 0; i < length - 1; i++) {
+		for (j = 0; j < length -1- i; j++) {
+			if (A[j] > A[j + 1]) swap(&A[j], &A[j + 1]);
+		}
+	}
+}
+
+int main() {
+	int A[100];
+	int i, length;
+	int num;
+	scanf_s("%d", &num);
+	for (i = 0; i < 100 && num != -1; i++) {
+		A[i] = num;
+		scanf_s("%d", &num);
+	}
+	length = i;
+	Bubblesort(A, length);
+	for (i = 0; i < length; i++) {
+		printf("%d ", A[i]);
+	}
+	return 0;
+}
